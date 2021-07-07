11c300fc6d20cdf8c25812a805c34b75c3dfe847
Deivid Gabriel <deividgabrielproc@gmail.com>
2021-07-07 12:41:08 -0300

    update: Added gRPC and Kafka (2 class)

--- codebank/go.mod
+++ codebank/go.mod
@@ -3,6 +3,14 @@ module github.com/Fukubi/codebank
 go 1.15
 
 require (
+	github.com/confluentinc/confluent-kafka-go v1.7.0
+	github.com/golang/protobuf v1.5.2
+	github.com/joho/godotenv v1.3.0
 	github.com/lib/pq v1.10.2
 	github.com/satori/go.uuid v1.2.0
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	google.golang.org/genproto v0.0.0-20210701191553-46259e63a0a9 // indirect
+	google.golang.org/grpc v1.39.0
+	google.golang.org/protobuf v1.27.1
 )
--- codebank/usecase/process_transaction.go
+++ codebank/usecase/process_transaction.go
@@ -1,21 +1,25 @@
 package usecase
 
 import (
+	"encoding/json"
+	"os"
 	"time"
 
 	"github.com/Fukubi/codebank/domain"
 	"github.com/Fukubi/codebank/dto"
+	"github.com/Fukubi/codebank/infrastructure/kafka"
 )
 
 type UseCaseTransaction struct {
 	TransactionRepository domain.TransactionRepository
+	KafkaProducer         kafka.KafkaProducer
 }
 
 func NewUseCaseTransaction(transactionRepository domain.TransactionRepository) UseCaseTransaction {
 	return UseCaseTransaction{TransactionRepository: transactionRepository}
 }
 
-func (u UseCaseTransaction) processTransaction(transactionDto dto.Transaction) (domain.Transaction, error) {
+func (u UseCaseTransaction) ProcessTransaction(transactionDto dto.Transaction) (domain.Transaction, error) {
 	creditCard := u.hydrateCreditCard(transactionDto)
 	ccBalanceAndLimit, err := u.TransactionRepository.GetCreditCard(*creditCard)
 	if err != nil {
@@ -30,6 +34,17 @@ func (u UseCaseTransaction) processTransaction(transactionDto dto.Transaction) (
 	if err != nil {
 		return domain.Transaction{}, err
 	}
+	transactionDto.ID = t.ID
+	transactionDto.CreatedAt = t.CreatedAt
+	transactionJson, err := json.Marshal(transactionDto)
+	if err != nil {
+		return domain.Transaction{}, err
+	}
+	err = u.KafkaProducer.Publish(string(transactionJson), os.Getenv("KafkaTransactionsTopic"))
+	if err != nil {
+		return domain.Transaction{}, err
+	}
+
 	return *t, nil
 }
 
